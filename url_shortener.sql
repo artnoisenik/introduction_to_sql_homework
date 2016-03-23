660681969ca8a288ef3a859785cb69e2ad11bdd5
Phil Skaggs <prskaggsjr@gmail.com>
2016-03-23 13:29:18 -0600

    added five data rows

--- url_shortener.sql
+++ url_shortener.sql
@@ -5,5 +5,12 @@ CREATE DATABASE url_shortener_phil_skaggs;
 CREATE TABLE urls(
   id SERIAL PRIMARY KEY,
   original_url VARCHAR(100) NOT NULL,
-  count INT,
+  count INT DEFAULT 0
 );
+
+INSERT INTO urls (original_url,count)
+VALUES ('http://www.boingboing.net',12),
+('http://www.baconbeercan.com/',23),
+('http://www.hairpieceherpes.com/',99),
+('http://www.whattoexpect.com/forums/postpartum-depression.html', 1),
+('https://www.okcupid.com/',9);
